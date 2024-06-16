e0a2395a9a0d559e04b1bd361e3ffed6b5382454
Ananya Verma <ananyaa2311@gmail.com>
2024-06-17 01:37:51 +0530

    whitelist clientside url on backend

--- server/index.js
+++ server/index.js
@@ -33,7 +33,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://connect-social-media-app.netlify.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  }
+));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
